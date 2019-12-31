b2d1c25b63aa0610879142552a21e2c29db9beea
stefans <stefan.selvarajoo@gmail.com>
2019-12-31 23:37:14 +0800

    Update register.php

--- register.php
+++ register.php
@@ -1,9 +1,6 @@
 <?php
 // Change this to your connection info.
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'phplogin';
+$con = OpenCon();
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
