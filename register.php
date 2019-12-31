07f216a1f1683a66561fde5492298151f7db88fe
stefans <stefan.selvarajoo@gmail.com>
2019-12-31 23:38:58 +0800

    Update register.php

--- register.php
+++ register.php
@@ -36,6 +36,6 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
 	// Something is wrong with the sql statement, check to make sure accounts table exists with all 3 fields.
 	echo 'Could not prepare statement!';
 }
-$con->close();
+CloseCon($con);
 }
 ?>
