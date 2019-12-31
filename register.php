dbe22fc5279b2ee6607ebaf9f6c094b18e5b43a1
stefans <stefan.selvarajoo@gmail.com>
2019-12-31 23:37:47 +0800

    Update register.php

--- register.php
+++ register.php
@@ -6,4 +6,15 @@ $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_
 if (mysqli_connect_errno()) {
 	// If there is an error with the connection, stop the script and display the error.
 	die ('Failed to connect to MySQL: ' . mysqli_connect_error());
+	
+	// Now we check if the data was submitted, isset() function will check if the data exists.
+if (!isset($_POST['username'], $_POST['password'], $_POST['email'])) {
+	// Could not get the data that should have been sent.
+	die ('Please complete the registration form!');
+}
+// Make sure the submitted registration values are not empty.
+if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['email'])) {
+	// One or more values are empty.
+	die ('Please complete the registration form');
+}
 }
