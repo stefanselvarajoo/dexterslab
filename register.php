9a35b440d33bf7c0d68637c50d0727acc8ae2e5a
stefans <stefan.selvarajoo@gmail.com>
2019-12-31 23:38:37 +0800

    Update register.php

--- register.php
+++ register.php
@@ -17,4 +17,25 @@ if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['emai
 	// One or more values are empty.
 	die ('Please complete the registration form');
 }
+
+// We need to check if the account with that username exists.
+if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+	// Bind parameters (s = string, i = int, b = blob, etc), hash the password using the PHP password_hash function.
+	$stmt->bind_param('s', $_POST['username']);
+	$stmt->execute();
+	$stmt->store_result();
+	// Store the result so we can check if the account exists in the database.
+	if ($stmt->num_rows > 0) {
+		// Username already exists
+		echo 'Username exists, please choose another!';
+	} else {
+		// Insert new account
+	}
+	$stmt->close();
+} else {
+	// Something is wrong with the sql statement, check to make sure accounts table exists with all 3 fields.
+	echo 'Could not prepare statement!';
 }
+$con->close();
+}
+?>
