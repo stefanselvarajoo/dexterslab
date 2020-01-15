f60ee6d1f148b246963ec815513ab98970d715c9
Stefan Selvarajoo <stefan.selvarajoo@gmail.com>
2020-01-15 11:33:27 +0800

    Update findSlot.php

--- findSlot.php
+++ findSlot.php
@@ -5,5 +5,14 @@ session_start();
 // Change this to your connection info.
 $con = OpenCon();
 
+// Prepare our SQL, preparing the SQL statement will prevent SQL injection.
+if ($stmt = $con->prepare('SELECT rackID,slotID FROM rack WHERE slotID < 32')) {
+	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
+	$stmt->bind_param('s', $_POST['rackID']);
+	$stmt->execute();
+	// Store the result so we can check if the account exists in the database.
+	$stmt->store_result();
+}
+
 CloseCon($con);
 ?>
