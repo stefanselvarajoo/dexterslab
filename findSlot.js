3612dab8bcdf0a64577de4ecfda975dbd3759440
Stefan Selvarajoo <stefan.selvarajoo@gmail.com>
2020-01-17 17:05:28 +0800

    Update findSlot.js

--- findSlot.js
+++ findSlot.js
@@ -1,13 +1,17 @@
 "use strict";
 
-// let oReq = new XMLHttpRequest();
+function getSlot(data){
+	return null;
+}
 
-// oReq.open("GET","findSlot.php",true);
-
-// oReq.send();
-
+/* https://developer.mozilla.org/en-US/docs/Web/API/Body/json */
+/* https://zellwk.com/blog/looping-through-js-objects/ */
 fetch("findSlot.php").then(function(response) {
-  response.text().then(function(text) {
-    console.log(text);
+  response.json().then(function(data) {
+    for(const val of Object.entries(data)){
+		//val[0] represents the rackID, val[1] represents slots available
+		(getSlot(parseInt(val[1]).toString(2) , 2) == null) ? console.log("hey") : console.log("test");
+		
+	}
   });
 });
