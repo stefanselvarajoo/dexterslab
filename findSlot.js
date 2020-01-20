5c927bb94530596eab0a0fad2550316d9e26638f
stefans <stefan.selvarajoo@gmail.com>
2020-01-20 18:23:10 +0800

    Update findSlot.js

--- findSlot.js
+++ findSlot.js
@@ -1,5 +1,7 @@
 "use strict";
 
+/* find the exact slot(s) from the rack which are available */
+/* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint32Array */
 function getSlot(db,slot){
 	let binVal = new Uint32Array(db);
 
@@ -16,22 +18,29 @@ function getSlot(db,slot){
 			console.log("found");
 			return i;
 		}
+		else return 0;
 	}
 }
 
+let rack = {
+	rackID:0,
+	index: []
+};
 
-let rackID = 0;
 /* https://developer.mozilla.org/en-US/docs/Web/API/Body/json */
 /* https://zellwk.com/blog/looping-through-js-objects/ */
 fetch("findSlot.php").then(function(response) {
   response.json().then(function(data) {
+	let foundRack = Object.create(rack);
     for(const val of Object.entries(data)){
 		//val[0] represents the rackID, val[1] represents slots available
-
-		let index = getSlot(parseInt(val[1]) , 2);
-		rackID = val[0];
 		
-		
+		let index = getSlot(parseInt(val[1]) , panel);
+		for(let i=0;i<panel;i++){
+			foundRack.index = index;
+			index--;
+		}
+		foundRack.rackID = val[0];
 	}
   });
 });
