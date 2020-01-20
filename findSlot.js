ca6a2e3ee41eacc63b9fe19a54bdba47a8078121
stefans <stefan.selvarajoo@gmail.com>
2020-01-20 15:30:07 +0800

    Update findSlot.js

--- findSlot.js
+++ findSlot.js
@@ -19,6 +19,8 @@ function getSlot(db,slot){
 	}
 }
 
+
+let rackID = 0;
 /* https://developer.mozilla.org/en-US/docs/Web/API/Body/json */
 /* https://zellwk.com/blog/looping-through-js-objects/ */
 fetch("findSlot.php").then(function(response) {
@@ -27,7 +29,7 @@ fetch("findSlot.php").then(function(response) {
 		//val[0] represents the rackID, val[1] represents slots available
 
 		let index = getSlot(parseInt(val[1]) , 2);
-		const rackID = val[0];
+		rackID = val[0];
 		
 		
 	}
